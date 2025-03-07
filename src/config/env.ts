a5229e20c0dd18ed2454b27c89d801b14919ae9f
= <raphy0316@naver.com>
2025-03-07 04:22:45 -0600

    Change Structure

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,7 +6,5 @@ export const ENV = {
     PORT: process.env.PORT || 3000,
     MADGRADES_API_BASE_URL: process.env.MADGRADES_API_BASE_URL || "",
     API_TOKEN: process.env.MADGRADES_API_TOKEN || "",
-    FIREBASE_CREDENTIALS: JSON.parse(process.env.FIREBASE_CREDENTIALS as string),
-    FIREBASE_URL : process.env.FIREBASE_URL,
     DATABASE_URL : process.env.DATABASE_URL
 };
